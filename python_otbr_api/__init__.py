47462373b7a58cc5947911fecece8202dafe68a2
Erik Montnemery <erik@montnemery.com>
2023-02-03 09:40:45 +0100

    Allow enabling the OTBR and create an active dataset (#7)
    * Allow enabling the OTBR and create an active dataset
    
    * Add TLV version
    
    * Mypy
    
    * pylint
    
    * Don't double quote
    
    * Replace create_active_dataset_tlvs with set_active_dataset_tlvs

--- python_otbr_api/__init__.py
+++ python_otbr_api/__init__.py
@@ -4,9 +4,15 @@ from http import HTTPStatus
 
 import aiohttp
 
+from .models import OperationalDataSet
+
 
 class OTBRError(Exception):
     """Raised on error."""
+
+
+class ThreadNetworkActiveError(OTBRError):
+    """Raised on attempts to modify the active dataset when thread network is active."""
 
 
 class OTBR:  # pylint: disable=too-few-public-methods
@@ -20,6 +26,18 @@ class OTBR:  # pylint: disable=too-few-public-methods
         self._url = url
         self._timeout = timeout
 
+    async def set_enabled(self, enabled: bool) -> None:
+        """Enable or disable the router."""
+
+        response = await self._session.post(
+            f"{self._url}/node/state",
+            json="enabled" if enabled else "disabled",
+            timeout=aiohttp.ClientTimeout(total=10),
+        )
+
+        if response.status != HTTPStatus.OK:
+            raise OTBRError(f"unexpected http status {response.status}")
+
     async def get_active_dataset_tlvs(self) -> bytes | None:
         """Get current active operational dataset in TLVS format, or None.
 
@@ -29,7 +47,6 @@ class OTBR:  # pylint: disable=too-few-public-methods
         response = await self._session.get(
             f"{self._url}/node/dataset/active",
             headers={"Accept": "text/plain"},
-            raise_for_status=True,
             timeout=aiohttp.ClientTimeout(total=self._timeout),
         )
 
@@ -43,3 +60,40 @@ class OTBR:  # pylint: disable=too-few-public-methods
             return bytes.fromhex(await response.text("ASCII"))
         except ValueError as exc:
             raise OTBRError("unexpected API response") from exc
+
+    async def create_active_dataset(self, dataset: OperationalDataSet) -> None:
+        """Create active operational dataset.
+
+        The passed in OperationalDataSet does not need to be fully populated, any fields
+        not set will be automatically set by the open thread border router.
+        Raises if the http status is 400 or higher or if the response is invalid.
+        """
+
+        response = await self._session.post(
+            f"{self._url}/node/dataset/active",
+            json=dataset.as_json(),
+            timeout=aiohttp.ClientTimeout(total=self._timeout),
+        )
+
+        if response.status == HTTPStatus.CONFLICT:
+            raise ThreadNetworkActiveError
+        if response.status != HTTPStatus.ACCEPTED:
+            raise OTBRError(f"unexpected http status {response.status}")
+
+    async def set_active_dataset_tlvs(self, dataset: bytes) -> None:
+        """Set current active operational dataset.
+
+        Raises if the http status is 400 or higher or if the response is invalid.
+        """
+
+        response = await self._session.put(
+            f"{self._url}/node/dataset/active",
+            data=dataset.hex(),
+            headers={"Content-Type": "text/plain"},
+            timeout=aiohttp.ClientTimeout(total=10),
+        )
+
+        if response.status == HTTPStatus.CONFLICT:
+            raise ThreadNetworkActiveError
+        if response.status != HTTPStatus.ACCEPTED:
+            raise OTBRError(f"unexpected http status {response.status}")
